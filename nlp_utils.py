31548a6a82641f1717540de157f0fb43129018b7
Martin Villalba <villalba@7c0h.com>
2023-05-05 16:46:10 +0200

    Changes the signature of the functions in this library because some of them are not needed

--- nlp_utils.py
+++ nlp_utils.py
@@ -1,18 +1,10 @@
-import pygame
-import tempfile
-import time
-from llama_cpp import Llama
-from ovos_tts_plugin_mimic3 import Mimic3TTSPlugin
-from pygame.mixer import Sound
 
-def reply(llm, initial_prompt, conversation_log, context_turns=5, username="User"):
+def build_reply_prompt(initial_prompt, conversation_log, context_turns=5, username="User"):
     """ Generates an LLM reply for a given conversation. The conversation
     should be structured such that it is now the LLM's turn.
 
     Parameters
     ----------
-    llm : Llama()
-        Language model used to generate the responses
     initial_prompt : str
         Text used at the beginning of the prompt, right before the dialog.
     conversation_log : list(str)
@@ -29,8 +21,8 @@ def reply(llm, initial_prompt, conversation_log, context_turns=5, username="User
     str
         An utterance that the AI generates in response to the given dialog.
     """
-    assert len(conversation_log) % 2 == 1,
-           "The conversation should start and end with the user"
+    assert len(conversation_log) % 2 == 1, \
+        "The conversation should start and end with the user"
     prompt = initial_prompt.format(username=username)
     user_turn = True
     subset = 1+2*context_turns
@@ -46,7 +38,7 @@ def reply(llm, initial_prompt, conversation_log, context_turns=5, username="User
     return prompt
 
 
-def broadcast(llm, initial_prompt, speech_log, context_turns=10, username="User"):
+def broadcast_prompt(initial_prompt, speech_log, context_turns=10, username="User"):
     """ Generates an LLM reply for a given broadcast.
 
     Parameters
@@ -70,8 +62,7 @@ def broadcast(llm, initial_prompt, speech_log, context_turns=10, username="User"
         history.
     """
     prompt = initial_prompt.format(username=username)
-    for utterance in conversation_log[-context_turns:]:
+    for utterance in speech_log[-context_turns:]:
         prompt += "I: {utterance}\n"
     prompt += "I: "
     return prompt
-
